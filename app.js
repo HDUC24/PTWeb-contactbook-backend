19c63cd73151d4646deb4af1a14446d373806190
HDUC24 <ducb2017034@student.ctu.edu.vn>
2024-02-23 09:03:08 +0700

    Dinh nghia cac route cho tai nguyen contact

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const cort = require("cors");
-
+const contactsRouter = require("./app/routes/contact.route");
 const app = express();
 
 app.use(cort());
 app.use(express.json());
-
+app.use("api/contacts", contactsRouter);
 app.post("/login", (req, res) => {
   res.json({ message: "Welcome to congact book application. " });
 });
